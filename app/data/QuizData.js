8aeba45c67a06ac5b0039be7a38174fab3d86992
gonzaloogv <138737563+gonzaloogv@users.noreply.github.com>
2024-06-02 23:00:05 -0300

    Add files via upload

--- app/data/QuizData.js
+++ app/data/QuizData.js
@@ -1,22 +1,27 @@
 export default data = [
     {
-        question: "Quien es el mas fachero del curso?",
-        options: ["Gonzalo","Nahuel","Santiago","Nuria","Cristian"],
-        correct_option: "Gonzalo"
+        question: "¿En qué año se celebró la primera edición de la Bienal del Chaco?",
+        options: ["1976","1982","1988","1991","1994"],
+        correct_option: "1988"
     },
     {
-        question: "Quien es el mas inteligente?",
-        options: ["Nuria","Santiago","Nahuel","Gonzalo","Cristian"],
-        correct_option: "Gonzalo"
+        question: "¿Quién es el fundador de la Bienal del Chaco?",
+        options: ["Fabriciano Gómez y Mimo Eidman","Gustavo Martinez y Jorge Capitanich","Aida Ayala","Javier Milei","Deolindo Felipe Bittel"],
+        correct_option: "Fabriciano Gómez y Mimo Eidman"
     },
     {
-        question: "Quien es el mas gracioso?",
-        options: ["Santiago","Nuria","Cristian","Gonzalo","Nahuel"],
-        correct_option: "Gonzalo"
+        question: "¿Cada cuánto tiempo se celebra la Bienal del Chaco?",
+        options: ["2 meses","4 años","6 meses","1 año","2 años"],
+        correct_option: "2 años"
     },
     {
-        question: "Quien es el mas capo?",
-        options: ["Cristian","Nahuel","Santiago","Nuria","Gonzalo"],
-        correct_option: "Gonzalo"
+        question: "¿Como se denominan a las manifestaciones artísticas que surgen a partir del siglo XX?",
+        options: ["Arte callejero","Arte pop","Arte moderno","Arte contemporáneo","Surrealismo"],
+        correct_option: "Arte contemporáneo"
+    },
+    {
+        question: "¿Cuál es el material seleccionado de esta edición 2024?",
+        options: ["Yeso","Roca","Madera","Metal","Arcilla"],
+        correct_option: "Metal"
     },
 ]
